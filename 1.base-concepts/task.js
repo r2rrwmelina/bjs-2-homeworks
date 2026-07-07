2a03bd45f04f672d447f80aba38e4ecc46ac5e5b
MG <gmelina69@gmail.com>
2026-07-07 19:48:48 +0400

    Add task 1 & 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - (4 * a * c);
+	let rootFormula = -b / (2 * a);
+	let formula1 = (-b + Math.sqrt(d)) / (2 * a);
+	let formula2 = (-b - Math.sqrt(d)) / (2 * a);
+	if (d < 0) {} else if (d === 0) {
+		arr.push(rootFormula);
+	} else if (d > 0) {
+		arr.push(formula1);
+		arr.push(formula2);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let rate = percent / 100 / 12;
+	let loanBody = amount - contribution;
+	let payment = loanBody * (rate + (rate / (((Math.pow(1 + rate, countMonths)) - 1))));
+	let totalLoanAmount = (payment * countMonths);
+	return Number(totalLoanAmount.toFixed(2));
 }

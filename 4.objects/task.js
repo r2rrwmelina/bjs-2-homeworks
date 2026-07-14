4e60060244033576e8a6f10b8d0f61c58e304cf5
MG <gmelina69@gmail.com>
2026-07-14 19:09:30 +0400

    Add tasks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	return this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) {
+		return;
+	}
+	return this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+	return this.marks.reduce((acc, mark, index, arr) => acc + mark / arr.length, 0);
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	return this.excluded = reason;
 }
